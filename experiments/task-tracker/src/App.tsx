a63a895aa4e77e075d63e2fdc8821a1d5a4f6dc9
John Dunavent <jdunaven@amazon.com>
2025-05-26 07:54:24 -0700

    Center task tracker app layout

--- experiments/task-tracker/src/App.tsx
+++ experiments/task-tracker/src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    <div className={styles.root}>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
   );
 }
 
